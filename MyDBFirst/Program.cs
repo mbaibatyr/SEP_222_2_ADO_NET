d6015c37491d1fb06b8dcd41d9f22da2765cb904
mbaibatyr <murat_b@mail.ru>
2023-11-17 20:44:18 +0600

    Добавьте файлы проекта.

---
+++ MyDBFirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace MyDBFirst
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            SqlDB db = new SqlDB();
+            db.TestConnection();            
+        }
+    }
+}
---
+++ MyDBFirst/SqlDB.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace MyDBFirst
+{
+    internal class SqlDB
+    {
+        private readonly string conStr = @"Server=221-13\MSSQLSERVER11;Database=SEP_222;Trusted_Connection=True;";
+        private readonly string conStr2 = @"Server=221-13\MSSQLSERVER11;Database=SEP_222;User Id=admin;Password=1234;";
+
+        public void TestConnection()
+        {
+            //using (OracleConnection db = new OracleConnection())
+            //{
+            //    db.Open();
+            //    Console.WriteLine(db.State);
+            //    db.Close();
+            //    Console.WriteLine(db.State);
+            //}
+
+
+
+            //using (NpgsqlConnection db = new NpgsqlConnection())
+            //{
+            //    db.Open();
+            //    Console.WriteLine(db.State);
+            //    db.Close();
+            //    Console.WriteLine(db.State);
+            //}
+
+
+            using (SqlConnection db = new SqlConnection(conStr2))
+            {
+                db.Open();
+                Console.WriteLine(db.State);
+                db.Close();
+                Console.WriteLine(db.State);
+            }
+        }
+    }
+}
