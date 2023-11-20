99b0d4357ec6d38f264345aa52143b8bc1f5592b
mbaibatyr <murat_b@mail.ru>
2023-11-20 19:50:38 +0600

    ---

--- MyDBFirst/SqlDB.cs
+++ MyDBFirst/SqlDB.cs
@@ -14,6 +14,10 @@ namespace MyDBFirst
         private readonly string conStr = @"Server=221-13\MSSQLSERVER11;Database=SEP_222;Trusted_Connection=True;";
         private readonly string conStr2 = @"Server=221-13\MSSQLSERVER11;Database=SEP_222;User Id=admin;Password=1234;";
 
+        private readonly string sql_1 = "SELECT  m.id, convert(varchar(10), m.[match_date], 104) dt, t1.[team_name] team_name_1, t2.[team_name] team_name_2, cast(m.[first_team_point] as varchar(2)) + ':' + cast(m.[second_team_point] as varchar(2)) score FROM [dbo].matches m join [dbo].[Teams] t1 on m.first_team_id = t1.id join [dbo].[Teams] t2 on m.second_team_id = t2.id";
+        private readonly string sql_2 = "SELECT [id] ,[dt] ,[team_name_1] ,[team_name_2] ,[score] FROM [testDB].[dbo].[vGetMatchesOfTeams]";
+        private readonly string sql_3 = "pGetMatchesOfTeams";
+
         public void TestConnection()
         {
             //using (OracleConnection db = new OracleConnection())
@@ -78,6 +82,8 @@ namespace MyDBFirst
         //}
 
         }
+
+
     }
 }
 
