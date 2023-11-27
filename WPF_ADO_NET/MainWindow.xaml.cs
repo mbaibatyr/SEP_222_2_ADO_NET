5ba8f1ad2ee9842c34597135be2d56552ef41fc5
mbaibatyr <murat_b@mail.ru>
2023-11-27 19:43:06 +0600

    --

--- WPF_ADO_NET/MainWindow.xaml.cs
+++ WPF_ADO_NET/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,29 @@ namespace WPF_ADO_NET
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection db;
         public MainWindow()
         {
             InitializeComponent();
+            db = new SqlConnection(ConfigurationManager.AppSettings["db"]);
+            db.Open();
+        }
+
+        private void btFind_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("pStaffInfo", db))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@company_name", tbCompany.Text);
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dgStaff.ItemsSource = dt;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            db.Close();
         }
     }
 }
---
+++ WPF_ADO_NET/Model/StaffInfoModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_ADO_NET.Model
+{
+    internal class StaffInfoModel
+    {
+        public string l_name { get; set; }
+        public string f_name { get; set; }
+        public string date_birth { get; set; }
+        public string salary { get; set; }
+        public string gender { get; set; }
+        public string position { get; set; }
+        public string company { get; set; }
+        public string head_office_location { get; set; }
+        public string date_foundation { get; set; }
+        public string cost { get; set; }
+        public string ceo { get; set; }
+
+        
+
+    }
+}
