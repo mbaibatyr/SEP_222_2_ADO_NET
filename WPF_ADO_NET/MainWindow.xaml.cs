184a4ba0b1406c7fce8b1f671d8f774bb0d2749d
mbaibatyr <murat_b@mail.ru>
2023-11-27 19:51:49 +0600

    --

--- WPF_ADO_NET/MainWindow.xaml.cs
+++ WPF_ADO_NET/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_ADO_NET.Model;
 
 namespace WPF_ADO_NET
 {
@@ -28,7 +29,7 @@ namespace WPF_ADO_NET
         {
             InitializeComponent();
             db = new SqlConnection(ConfigurationManager.AppSettings["db"]);
-            db.Open();
+            db.Open();            
         }
 
         private void btFind_Click(object sender, RoutedEventArgs e)
@@ -39,13 +40,30 @@ namespace WPF_ADO_NET
                 cmd.Parameters.AddWithValue("@company_name", tbCompany.Text);
                 DataTable dt = new DataTable();
                 dt.Load(cmd.ExecuteReader());
-                dgStaff.ItemsSource = dt;
+
+                List<StaffInfoModel> lst = (from DataRow dr in dt.Rows
+                               select new StaffInfoModel()
+                               {
+                                   ceo = dr["ceo"].ToString(),
+                                   company = dr["company"].ToString(),
+                                   cost = dr["cost"].ToString(),
+                                   date_birth = dr["date_birth"].ToString(),
+                                   date_foundation = dr["date_foundation"].ToString(),
+                                   f_name = dr["f_name"].ToString(),
+                                   gender = dr["gender"].ToString(),
+                                   head_office_location = dr["head_office_location"].ToString(),
+                                   l_name = dr["l_name"].ToString(),
+                                   position = dr["position"].ToString(),
+                                   salary = dr["salary"].ToString()                                   
+                               }).ToList();
+
+                dgStaff.ItemsSource = lst;
             }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             db.Close();
-        }
+        }      
     }
 }
